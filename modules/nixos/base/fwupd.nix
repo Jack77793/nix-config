b337f0bb80ed750e5ac7228ab638477120cc84cf
Jack77793 <jack77793@hotmail.com>
2026-05-20 11:39:59 +0800

    fix(fwupd): temporary workaround to patch EFI app path for lanzaboote
    ref: https://github.com/nix-community/lanzaboote/issues/591

--- modules/nixos/base/fwupd.nix
+++ modules/nixos/base/fwupd.nix
@@ -1,6 +1,18 @@
+{ pkgs, ... }:
+
 {
   services.fwupd = {
     enable = true;
+    package = (
+      pkgs.fwupd.overrideAttrs (old: {
+        postPatch = (old.postPatch or "") + ''
+          substituteInPlace meson.build \
+            --replace-fail \
+            "efi_app_location = join_paths(dependency('fwupd-efi').get_variable(pkgconfig: 'prefix'), 'libexec', 'fwupd', 'efi')" \
+            "efi_app_location = '/run/fwupd-efi'"
+        '';
+      })
+    );
   };
 
   environment.persistence."/nix/persist".directories = [
