9d237fcce6c8cca0e48819f62a54928496441bec
Jack77793 <jack77793@hotmail.com>
2026-05-23 18:47:22 +0800

    feat(xdg): expand MIME type default applications

--- modules/nixos/desktop/xdg.nix
+++ modules/nixos/desktop/xdg.nix
@@ -5,10 +5,31 @@
     mime = {
       addedAssociations = {
         "text/*" = "nvim.desktop";
+        "audio/midi" = "org.gnome.Decibels.desktop";
       };
       defaultApplications = {
-        "text/*" = "nvim.desktop";
+        "audio/*" = "org.gnome.Decibels.desktop";
         "image/*" = "org.gnome.Loupe.desktop";
+        "text/*" = "nvim.desktop";
+        "video/*" = "mpv.desktop";
+
+        "application/json" = "nvim.desktop";
+        "application/x-desktop" = "nvim.desktop";
+        "application/x-shellscript" = "nvim.desktop";
+        "application/xml" = "nvim.desktop";
+        "application/yaml" = "nvim.desktop";
+
+        "application/*-compressed-tar" = "org.gnome.FileRoller.desktop";
+        "application/java-archive" = "org.gnome.FileRoller.desktop";
+        "application/vnd.rar" = "org.gnome.FileRoller.desktop";
+        "application/x-7z-compressed" = "org.gnome.FileRoller.desktop";
+        "application/x-rpm" = "org.gnome.FileRoller.desktop";
+        "application/zip" = "org.gnome.FileRoller.desktop";
+
+        "application/epub+zip" = "com.github.johnfactotum.Foliate.desktop";
+        "application/pdf" = "org.gnome.Evince.desktop";
+        "application/x-musescore" = "org.musescore.MuseScore.desktop";
+        "text/x-bibtex" = "org.gnome.World.Citations.desktop";
       };
     };
     portal = {
