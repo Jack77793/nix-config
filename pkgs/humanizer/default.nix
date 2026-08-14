4f24462db556f6bc32dd102e5a66b72445a14983
Jack77793 <jack77793@hotmail.com>
2026-08-15 01:18:12 +0800

    pkgs(humanizer): bump to 20260722

--- pkgs/humanizer/default.nix
+++ pkgs/humanizer/default.nix
@@ -2,10 +2,10 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "humanizer";
-  version = "20260607";
+  version = "20260722";
   src = fetchurl {
-    url = "https://raw.githubusercontent.com/blader/humanizer/9600f2b7241cb4eed6ad803abee5ea01d67fe8e4/SKILL.md";
-    sha256 = "18za2jhq21hbxxbr2xdvnibgkgw8jxivl7r8zwy4qgm6jvfqbn1d";
+    url = "https://raw.githubusercontent.com/blader/humanizer/523374dee72d67c7b2b5f858ea0094ffda49c3ac/SKILL.md";
+    hash = "sha256-cJOPPM4llw4d7V/dGUt1XAPx5OT6doIJWM54+Gtnexo=";
   };
 
   dontUnpack = true;
