65448a1d7895b311c780392ad769207669b2a0b2
Jack77793 <jack77793@hotmail.com>
2026-09-10 09:43:44 +0800

    chore: switch nixpkgs to channel tarball fetcher, update flake.lock

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Jack77793's NixOS config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.zst";
 
     home-manager = {
       url = "github:nix-community/home-manager";
