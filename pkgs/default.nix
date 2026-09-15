f5138fda48cec7e94bd570f29ff51f9aeb84ee32
Jack77793 <jack77793@hotmail.com>
2026-09-15 15:40:54 +0800

    pkgs(dsh-context): new package, add 0.52.2

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,6 +2,7 @@
   (final: prev: {
     anthropics-skills = prev.callPackage ./anthropics-skills { };
     dsh = prev.callPackage ./dsh { };
+    dsh-context = prev.callPackage ./dsh-context { };
     eisvogel-template = prev.callPackage ./eisvogel-template { };
     humanizer = prev.callPackage ./humanizer { };
     humanizer-zh = prev.callPackage ./humanizer-zh { };
---
+++ pkgs/dsh-context/default.nix
@@ -0,0 +1,61 @@
+{
+  lib,
+  stdenvNoCC,
+  fetchFromGitHub,
+  fetchPnpmDeps,
+  pnpmConfigHook,
+  pnpm,
+  nodejs,
+  dsh,
+}:
+
+stdenvNoCC.mkDerivation rec {
+  pname = "dsh-context";
+  version = "0.52.2";
+
+  src = fetchFromGitHub {
+    owner = "bowenliang123";
+    repo = "dsh-context";
+    rev = "v${version}";
+    hash = "sha256-iF+RIrUux0C+E00q+nzDak+OJxGfXe0oUfkwuHWvK9I=";
+  };
+
+  pnpmDeps = fetchPnpmDeps {
+    inherit pname version src;
+    inherit pnpm;
+    fetcherVersion = 4;
+    hash = "sha256-LxTgpsv1jH15CnfPtJRDzP8igIf4pAvLIIlBNpV6KTA=";
+  };
+
+  nativeBuildInputs = [
+    nodejs
+    pnpm
+    pnpmConfigHook
+  ];
+
+  buildPhase = ''
+    runHook preBuild
+    pnpm run build
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    rm -rf node_modules
+    pnpm install --prod --offline --frozen-lockfile --ignore-scripts
+
+    mkdir -p $out
+    cp -r . $out/pkg
+    ln -s ${dsh}/apps/cli/node_modules $out/node_modules
+
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "dsh-context as a dsh profile bundle";
+    homepage = "https://github.com/bowenliang123/dsh-context";
+    license = lib.licenses.asl20;
+    platforms = [ "x86_64-linux" ];
+  };
+}
